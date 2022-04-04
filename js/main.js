648310d640bd1d59c15ddca8bf4ff11a2c85c7cc
Uri Baynesay <uri552@gmail.com>
2022-04-04 18:01:08 +0300

    started support for dynamic protfolio items(protfolio controller and service created)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 console.log('Starting up');
+
+$(document).ready(onInit);
+
+function onInit(){
+    debugger
+    createPortfolio();
+}
---
+++ js/services/portfolio.service.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var gPortf = [];
+
+function createPortfolio() {
+    gPortf.push(
+    _createPortfolio(
+      "MineSweeper",
+      "Careful! There are mines here",
+      'The objective of the game is to clear a rectangular board containing hidden "mines" or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field.',
+      "https://uribaynesay.github.io/Mine-Sweeper/",
+      ["Matrix", "Intervals", "Conditions"]
+    )
+  );
+//   gportf.push(_createPortfolio(projName, title, txt, url, labels));
+//   gportf.push(_createPortfolio(projName, title, txt, url, labels));
+//   gportf.push(_createPortfolio(projName, title, txt, url, labels));
+//   gportf.push(_createPortfolio(projName, title, txt, url, labels));
+//   gportf.push(_createPortfolio(projName, title, txt, url, labels));
+}
+
+function _createPortfolio(projName, title, txt, url, labels) {
+  return {
+    id: projName.toLowerCase(),
+    name: projName,
+    title,
+    desc: txt,
+    url: url,
+    publishedAt: Date.now(),
+    labels: labels,
+  };
+}
